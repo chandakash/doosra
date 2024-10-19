7c7f39428bd6cf1a1d731997d0fc5017c60a1bf3
Akash Chand <akashchand1703@gmail.com>
2024-10-19 20:53:30 +0530

    update: initial files with player and match module

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PlayerModule } from './player/player.module';
+import { MatchModule } from './match/match.module';
+
+@Module({
+  imports: [PlayerModule, MatchModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
